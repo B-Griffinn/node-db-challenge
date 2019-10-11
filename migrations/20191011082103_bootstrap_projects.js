4e7c3838c54f15bf34d78eb9eff4c77c71bd0d3d
Ben <bigriffin94@outlook.com>
2019-10-11 08:41:34 -0700

    Migration 1 Successful succesfully created the projects table with all correct columns

--- migrations/20191011082103_bootstrap_projects.js
+++ migrations/20191011082103_bootstrap_projects.js
@@ -1,8 +1,24 @@
 
 exports.up = function(knex) {
-  
+    // create a table and give it a name of projects
+  return knex.schema.createTable('projects', tbl => {
+      // create unique id column
+      tbl.increments();
+
+      // create name column = string, required
+      tbl.string('name', 255) // << character limit for postgreS
+        .notNullable();
+
+      // create description column - nullable
+      tbl.string('description', 500);
+
+      // create a boolean column that defaults to false/0 & is notNUllable
+      tbl.boolean('completed')
+        .notNullable()
+        .defaultTo(0);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('projects')
 };
